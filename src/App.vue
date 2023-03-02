4a00edd6b5fb8bf46cf74e5b682d3fa11cdb1abf
wangchuyuan <18262794229@163.com>
2023-03-02 19:56:24 +0800

    ceshi

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="elcome to Your Vue.js App"/>
+    <HelloWorld msg="welcome to Your Vue.js App"/>
   </div>
 </template>
 
